cf27bb292db5ec8591d4ea978d280b1885716e8b
evi1haxor <architdwivedi.off@gmail.com>
2020-03-27 19:57:41 +0530

    cleaned data generator

--- src/dfhandler.py
+++ src/dfhandler.py
@@ -4,6 +4,10 @@ Reads the csv and returns dataframe/Similarity
 
 # from . import matrices 
 import pandas as pd
+import sklearn.cluster as skl_cluster
+import sklearn.datasets as skl_data
+import numpy as np
+
 
 class dframe_csv:
     def __init__(self, path, mat_type):
@@ -13,3 +17,21 @@ class dframe_csv:
         self.nrow = self.df.shape[0]
         self.ncol = self.df.shape[1]
     
+def data_generator():
+    circles, circles_clusters = skl_data.make_circles(n_samples=400, noise=.01, random_state=0)
+
+    wrapy = [0]
+    wrapy[0]=[i for i in range(len(circles[0]))]
+    circles = np.vstack((wrapy, circles))
+    circles = np.transpose(circles)
+
+    wrapx = []
+    wrapx=[i for i in range(len(circles[0]))]
+    circles = np.vstack((wrapx, circles))
+
+    np.savetxt("toy_data.csv", circles, delimiter = ',')
+
+# generate_data()
+
+# df = pd.read_csv("toy_data.csv"    df = pd.read_csv("toy_data.csv")
+# print(df.shape)
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import sklearn.cluster as skl_cluster
 import sklearn.datasets as skl_data
-# import matplotlib.pylab as plt 
+import matplotlib.pylab as plt 
 import numpy as np
 import pandas as pd
 
@@ -22,34 +22,3 @@ def junk(): #uncomment to plot the comaprision b/w k mean and spectral clusterin
     labels = model.fit_predict(circles)
     plt.scatter(circles[:, 0], circles[:, 1], s=15, linewidth=0, c=labels, cmap='flag')
     plt.show()
-
-def generate_data():
-    circles, circles_clusters = skl_data.make_circles(n_samples=400, noise=.01, random_state=0)
-    # t_circles = np.transpose(circles)
-    # print(x1.degree, '\n', x1.W, '\n', x1.laplacian)
-    # result = circles[:,0]
-    wrapy = [0]
-    # a = [[11,21,31,14,15],[61,71,81,91,110]]
-    wrapy[0]=[i for i in range(len(circles[0]))]
-    circles = np.vstack((wrapy, circles))
-    circles = np.transpose(circles)
-
-    # print(a)
-    wrapx = []
-    wrapx=[i for i in range(len(circles[0]))]
-    circles = np.vstack((wrapx, circles))
-    # a = wrap2 + a 
-    
-    # wrap = np.arange(1, len(a[0])+1, 1)
-    # a = np.append(wrap, a, axis=1)
-    # a = np.append(wrap, a, axis=0)
-    # result = np.transpose(a)
-
-    # wrap = np.arange(1, len(a[0]), 1)
-    # a = np.append(wrap, a, axis=0)
-    np.savetxt("toy_data.csv", circles, delimiter = ',')
-
-# generate_data()
-
-df = pd.read_csv("toy_data.csv")
-print(df.shape)
