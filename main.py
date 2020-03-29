d4cceb760a45e4ec741211c1d0506f64aa93afe3
evi1haxor <architdwivedi.off@gmail.com>
2020-03-29 17:51:24 +0530

    made dummy laplacian for EVD testing

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ path = '/hdd/Ztudy/BTP/code/CoALa/algo/toy_data.csv'
 
 try:
     x1= src.modalities.modality(path, mat_type="gaussian")
-    print("made x1")
+    print("made x1. onto wrapping laplacian")
+    lap = src.tests.wrap_test(x1.laplacian, "columns")
+    np.savetxt("mi_lap.csv", lap, delimiter = ',')
 except: 
     print("NO PATH PROVIDED")
 
 # EIGEN DECOMPOSITION
-
-
-
