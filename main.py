0a73e2697693e974522c0359fbbc61089d94ad15
evi1haxor <architdwivedi.off@gmail.com>
2020-05-12 16:46:55 +0530

    test successful on bimodal problem

--- main.py
+++ main.py
@@ -25,10 +25,22 @@ l2=L2.to_numpy()
 
 lap = [l1, l2]
 
+
+Ls = src.modalities.lap_list(lap = lap, rank = 4)
+
+V = Ls.joint_eig_vectors
+V = V.real
+from sklearn.cluster import KMeans
+
+kmeans = KMeans(n_clusters=2, random_state=0).fit(V[:,:1])
+y_mean = kmeans.predict(V[:,:1])
+X1 = pd.read_csv("/hdd/Ztudy/BTP/code/CoALa/algo/.data/X1.csv")
+x1 = X1.to_numpy()
+import matplotlib.pyplot as plt
+plt.scatter(x1[0],x1[1], c=y_mean, s=50,cmap="viridis")
+plt.show()
 # Find alphas
 
-l1=0.5*l1
-l2=0.5*l2
  
 # make orthonormal basis
 
