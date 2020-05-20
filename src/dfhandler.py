b7c42e78e5dd2a86fc1ffd828cc4c1c046052ae6
evi1haxor <architdwivedi.off@gmail.com>
2020-05-20 17:13:27 +0530

    dfhandler modules regrouping

--- src/dfhandler.py
+++ src/dfhandler.py
@@ -1,14 +1,19 @@
 """
-Reads the csv and returns dataframe/Similarity
+Module to handle all dataframe related affairs. 
 """
 
-from . import helperFunc 
+from . import helpers
 import pandas as pd
 import sklearn.cluster as skl_cluster
 import sklearn.datasets as skl_data
 import numpy as np
 
-
+"""
+dfhandler.dframe_csv.__init__:
+    params:
+        path: absolute path to the datafile to be read (modality)
+        mat_type: type of similarity matrix you want for the the modality
+"""
 class dframe_csv:
     def __init__(self, path, mat_type):
         self.df = pd.read_csv(path)
@@ -17,17 +22,24 @@ class dframe_csv:
         self.nrow = self.df.shape[0]
         self.ncol = self.df.shape[1]
     
+"""
+dfhandler.data_generator: generates data which when plotted depicts two circular clusters 
+    params: 
+        multiplier: usefull in case multiple datafiles are requierd to be generated for experimentation
+        filename: file name of the generated datafile
+    return: a csv file gets stored
+"""
 def data_generator(multiplier = 1, filename = "toy.csv"):
     oc, circles_clusters = skl_data.make_circles(n_samples=400, noise=.01, random_state=0, factor = 0.5)
     oc = multiplier * oc
     circles = np.transpose(oc)
-    circles = helperFunc.wrapx(circles)
+    circles = helpers.house_keeper.wrapx(circles)
 
     np.savetxt(filename, circles, delimiter = ',')
     print("Saving toy data csv w/d", len(circles[:,0]))#, oc[0,:10])
     d = len(oc[0]) - len(circles[0])
-    # print(d)
-    return d
+
+    # return d
 
 # generate_data()
 
