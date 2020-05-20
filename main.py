73f871289b2cfb8b58e978e2bdad5eefc7c6443b
evi1haxor <architdwivedi.off@gmail.com>
2020-05-20 17:12:02 +0530

    generalised main

--- main.py
+++ main.py
@@ -5,22 +5,24 @@ from sklearn.cluster import KMeans
 import pandas as pd
 
 # READ MODALITIES
-path_list = ["",""] # add absolute path to data files here
+# add absolute path to data files here
+
+path_list = ["/hdd/Ztudy/BTP/code/CoALa/algo/.data/cleaned_mi.csv", "/hdd/Ztudy/BTP/code/CoALa/algo/.data/cleaned_mr.csv"] 
 
 lap= []
 
 try:
     for path in path_list:
         X= src.modalities.modality(path, mat_type="gaussian")
-        print("made X onto making laplacian")
+        print("Made a modality.")
         lap.append(X.laplacian)
-        print("laplacian appended successfully!")
+        print("Laplacian appended successfully!")
 except: 
     print("NO PATH PROVIDED")
 
 # expected number of clusters 
 k = 2
-rank = 4
+rank = 8
 
 Ls = src.modalities.lap_list(lap = lap, rank = rank)
 
@@ -30,15 +32,18 @@ V = V.real
 kmeans = KMeans(n_clusters=k, random_state=0).fit(V[:,:1])
 k_mean_affinity = kmeans.predict(V[:,:1])
 
-k_mean_affinity = np.array(k_mean_affinity) 
-
-CoALa = src.tests.wrap_test(k_mean_affinity, "columns")
-np.savetxt(".data/CoALa.csv" , CoALa , delimiter = ',')
-
-print("Computed data-point belonging info stored in CoALa.csv")
 # X1 = pd.read_csv(path_list[0])
 # x1 = X1.to_numpy()
 
 # import matplotlib.pyplot as plt
-# plt.scatter(x1[0],x1[1], c=k_mean_affinity, s=50,cmap="viridis")
+# plt.scatter(x1[0,0:],x1[1,0:], c=k_mean_affinity, s=50,cmap="viridis")
 # plt.show()
+
+
+k_mean_affinity = np.array(k_mean_affinity) 
+k_mean_affinity = k_mean_affinity + 1
+
+CoALa = src.helpers.helper.csv_wrapper(mat = k_mean_affinity, arg = "columns")
+np.savetxt(".data/mi_mr_CoALa.csv" , CoALa , delimiter = ',')
+
+print("Computed cluster info stored in .data/CoALa_mi_mr.csv")
