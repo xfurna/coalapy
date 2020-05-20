b333bacaa58cf7ca0a6dccf5209c70f231fddca0
evi1haxor <architdwivedi.off@gmail.com>
2020-05-21 05:27:56 +0530

    add class info in comments

--- src/modalities.py
+++ src/modalities.py
@@ -1,6 +1,10 @@
+"""
+Contains classes modality and lap_list 
+"""
+
+
 from . import dfhandler
 from . import helpers
-
 
 
 class modality:
@@ -12,18 +16,19 @@ class modality:
     def __get_laplacian(self, get = "shifted_laplacian"): #private function
         return helpers.helper.matrix(W = self.W, D = self.degree, get=get)
         
+
 class lap_list:
     def __init__(self , lap=None , rank=None):
         self.rank = rank
         self.M = len(lap)
         self.lap = lap
-        self.lra = self.__compute_lra()# define _compute_lra() within this class
-        self.weights = helpers.utils.get_weights(self.lap)# alphas from helpers
-        self.orthonorm_basis = helpers.utils.get_orthonorm_basis(lr_list = self.lra, rank = rank) # define make_orthonorm_basis() within this class
-        self.H = helpers.utils.get_H_matrix(orthonorm_basis = self.orthonorm_basis , lr_list = self.lra, rank = rank)  # define make_H() within the calss
-        self.rotation = self.__compute_rotation_matrix()  #define get_rotation()
-        self.joint_eig_vectors = self.__compute_eig_vectors() # multiply U and R
-    
+        self.lra = self.__compute_lra()
+        self.weights = helpers.utils.get_weights(self.lap)
+        self.orthonorm_basis = helpers.utils.get_orthonorm_basis(lr_list = self.lra, rank = rank) 
+        self.H = helpers.utils.get_H_matrix(orthonorm_basis = self.orthonorm_basis , lr_list = self.lra, rank = rank)
+        self.rotation = self.__compute_rotation_matrix()
+        self.joint_eig_vectors = self.__compute_eig_vectors()
+
     def __compute_lra(self):
         lap_r = []
         for i in range(self.M):
