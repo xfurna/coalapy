6277fdb487a1d26145c3c9e0ed71dd84e763371e
akashgarg98 <garg.5@iitj.ac.in>
2020-05-21 15:53:25 +0530

    cleaned alpha

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -49,9 +49,7 @@ helpers.utils.get_H_matrix(orthonorm_basis = None , lr_list = None , rank = 3): 
 
 from .. import matrices as Matrix
 import numpy as np
-import cmath
-from sklearn.metrics import silhouette_score 
-from sklearn.cluster import KMeans 
+
 
 
 def low_rank_mat(SVD=None, A=None, r=1):
@@ -108,24 +106,29 @@ def get_H_matrix(orthonorm_basis = None , lr_list = None , rank = 3):
     else:
         print("Provide list of lra laplacian matrices and orthonormal basis matrix")
 
-def alpha_(lap):
-    alpha_coala=[]
-    relevance=[]
-    for i in range(len(lap)):
-        eigenvalues,eigenvectors=LA.eig(lap[i])
+def compute_alpha(lap_list):
+    import cmath
+    from sklearn.metrics import silhouette_score 
+    from sklearn.cluster import KMeans 
+    numpy import linalg as LA
+    alpha_coala = []
+    relevance = []
+    for i in range(len(lap_list)):
+        eigenvalues , eigenvectors = LA.eig(lap[i])
         idx = np.argsort(eigenvalues)
         eigenvalues = eigenvalues[idx]
         eigenvectors = eigenvectors[ : ,idx]
-        fiedler=eigenvectors[:,:1]
+        fiedler = eigenvectors[: , :1]
         lambda2 = eigenvalues[1]  
-        fiedler=fiedler.real
+        fiedler = fiedler.real
             
-        kmeans = KMeans(n_clusters = 2,random_state=None).fit(fiedler[:,:])
+        kmeans = KMeans(n_clusters = 2,random_state = None).fit(fiedler[ : , : ])
         k_mean_affinity = kmeans.predict(fiedler[:,:])
         
         s_score = silhouette_score(fiedler, k_mean_affinity)
-        relevance .append(.25*(s_score*lambda2+1)*lambda2)
+        relevance .append(.25 * (s_score * lambda2 + 1) * lambda2)
         relevance.sort()
     for i in range (len(relevance)):
         alpha_coala.append(relevance[i]/pow(1,i))
     return alpha_coala
+
