e7b0ad9754393898de6c72a219ec888aa3115fa6
evi1haxor <architdwivedi.off@gmail.com>
2020-06-05 20:12:34 +0530

    changed description

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ setuptools.setup(
     version="0.0.1",
     author="evi1haxor",
     author_email="architdwivedi.off@gmail.com",
-    description="Paper implementation- CoALa",
+    description="A multimodal data clustering solution",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/evi1haxor/CoALa",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
 )
