e6fb2960149902a927489e4314ff0e83f92bffea
evi1haxor <architdwivedi.off@gmail.com>
2020-06-07 03:33:44 +0530

    renamed

--- sample.py
+++ sample.py
@@ -7,9 +7,12 @@ import pandas as pd
 # READ MODALITIES
 # add absolute path to data files here
 
-path_list = ["", ""]
-
+path_list = []
 lap = []
+
+# expected number of clusters
+k = 2
+rank = 8
 
 try:
     for path in path_list:
@@ -20,10 +23,6 @@ try:
 except:
     print("NO PATH PROVIDED")
 
-# expected number of clusters
-k = 2
-rank = 8
-
 Ls = coalapy.modalities.lap_list(lap=lap, rank=rank)
 
 V = Ls.joint_eig_vectors
@@ -32,18 +31,10 @@ V = V.real
 kmeans = KMeans(n_clusters=k, random_state=0).fit(V[:, :1])
 k_mean_affinity = kmeans.predict(V[:, :1])
 
-# X1 = pd.read_csv(path_list[0])
-# x1 = X1.to_numpy()
-
-# import matplotlib.pyplot as plt
-# plt.scatter(x1[0,0:],x1[1,0:], c=k_mean_affinity, s=50,cmap="viridis")
-# plt.show()
-
-
 k_mean_affinity = np.array(k_mean_affinity)
 k_mean_affinity = k_mean_affinity + 1
 
-# CoALa = coalapy.helpers.helper.csv_wrapper(mat=k_mean_affinity, arg="columns")
-# np.savetxt(".inventory/results.csv", CoALa, delimiter=",")
+CoALa = coalapy.helpers.helper.csv_wrapper(mat=k_mean_affinity, arg="columns")
+np.savetxt(".inventory/results.csv", CoALa, delimiter=",")
 
 print("Computed cluster info stored in .inventory/results.csv")
