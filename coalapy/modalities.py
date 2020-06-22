a055d805686a100a7a18a1f304a1351626b50dbd
evi1haxor <architdwivedi.off@gmail.com>
2020-06-22 07:26:24 +0530

    fix #23

--- coalapy/modalities.py
+++ coalapy/modalities.py
@@ -5,7 +5,7 @@ Contains classes modality and lap_list
 
 from . import dfhandler
 from . import helpers
-
+import numpy as np
 
 class modality:
     def __init__(self, path=None, mat_type="gaussian", clean=True):
@@ -20,8 +20,7 @@ class modality:
 
     def __get_laplacian(self, get="shifted_laplacian"):  # private function
         print("Returning laplacian...")
-        return helpers.helper.matrix(W=self.W, D=self.degree, get=get)
-
+        return np.around(helpers.helper.matrix(W=self.W, D=self.degree, get=get), decimals=9)
 
 class lap_list:
     def __init__(self, lap_full=None, rank=None, n_clusters=3):
@@ -31,13 +30,13 @@ class lap_list:
         self.lra = self.__compute_lra(lap_full)
         self.chi_list = helpers.utils.compute_chi(lap_full)
         self.lap = helpers.utils.sort_lr(self.chi_list, self.lra)
-        self.orthonorm_basis = helpers.utils.get_orthonorm_basis(
-            lap_list=self.lap_full, rank=rank
+        self.orthonorm_basis, self.Gamma = helpers.utils.get_orthonorm_basis(
+            lap_list=self.lap_full, chi_list=self.chi_list, rank=rank
         )
         self.H = helpers.utils.get_H_matrix(
-            orthonorm_basis=self.orthonorm_basis,
+            orthonorm_basis=self.Gamma,
             lr_list=self.lap,
-            chi_list=self.chi_list,
+            chi_list=helpers.utils.get_alpha(chi_list=self.chi_list),
             rank=rank,
         )
         self.rotation = self.__compute_rotation_matrix()
@@ -64,43 +63,3 @@ class lap_list:
         V = U.dot(R)
         print("computed joint-eigenvector-matrix")
         return V
-
-    # def plan_b(self, beta=1.25):
-    #     import numpy as np
-    #     alpha_list=self.chi_list.copy()
-    #     for i, chi in enumerate(alpha_list):
-    #         alpha_list[i]=chi/((beta)**(i+1))
-    #     alpha_list = [chi/np.sum(chi_list) for chi in alpha_list]
-    #     L = np.zeros(self.lap[0].shape)
-    #     for i,lr in enumerate(self.lap):
-    #         L+=lr*alpha_list[i]
-    #     return L
-
-    def get_alpha(self):
-        chi_ls = self.chi_list.copy()
-        for i, chi in enumerate(chi_ls):
-            chi_ls[i] = chi / ((1.25) ** (i + 1))
-        return chi_ls / sum(chi_ls)
-
-    # def compute_chi_alt(self, lap_list):
-    #     from sklearn.metrics import silhouette_score
-    #     from sklearn.cluster import KMeans
-    #     import numpy as np
-    #     chi_list = []
-    #     for lr in lap_list:
-    #         s, u = np.linalg.eig(lr)
-    #         s[np.where(s<1e-10)]=0
-    #         s=np.around(s, decimals=10)
-
-    #         ind = np.where(s == np.partition(s, -2)[-2])[0][0]
-
-    #         Y = s[ind].real
-    #         u = helpers.utils.sorted_u(lr).real
-    #         u_ = u[:,1].reshape(-1,1)
-    #         cluster = KMeans(n_clusters=2, random_state=None).fit(u_)
-    #         cluster_labels = cluster.predict(u_)
-    #         s_score = silhouette_score(u_, cluster_labels)
-
-    #         chi = s_score * np.absolute(Y)
-    #         chi_list.append(chi)
-    #     return chi_list
